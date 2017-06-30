2d5263f4dc980b9ef28e172ced60e56fc32a99b0
lixinwei <xinwei.li@winsuntech.cn>
2017-06-30 14:53:10 +0800

    update map.jsx

--- src/map.jsx
+++ src/map.jsx
@@ -9,53 +9,74 @@ class Map extends React.Component {
     }
 
     this.fire = () => {
-      this.setState({ success: true }, () => {
-        const map = new AMap.Map('mapContainer', {
-          resizeEnable: true,
-          zoom: 18
-        })
+      this.setState({ success: true }, this.getMap)
+    }
 
-        const lnglatXY = [this.props.longitude, this.props.latitude] // 已知点坐标
+    this.getMap = () => {
+      /* 已知点坐标 */
+      const lnglatXY = [this.props.longitude, this.props.latitude]
 
-        const geocoderCallBack = (data) => {
-          const address = data.regeocode.formattedAddress // 返回地址描述
-          console.log('address', data.regeocode, address)
-          if (this.props.resultId) document.getElementById(this.props.resultId).innerHTML = address
-        }
-
-        const regeocoder = () => {  // 逆地理编码
-          const geocoder = new AMap.Geocoder({
-            radius: 1000,
-            extensions: 'all'
-          })
-          geocoder.getAddress(lnglatXY, (status, result) => {
-            if (status === 'complete' && result.info === 'OK') {
-              geocoderCallBack(result)
-            }
-          })
-          const marker = new AMap.Marker({  // 加点
-            map,
-            position: lnglatXY
-          })
-          map.setFitView()
-        }
-
-        regeocoder()
+      const map = new AMap.Map('mapContainer', {
+        resizeEnable: true,
+        zoom: 15,
+        center: lnglatXY
       })
+
+      const geocoderCallBack = (data) => {
+        console.log('address', data.regeocode)
+        /* 返回地址描述 */
+        const compo = data.regeocode.addressComponent
+        const address = `${compo.province} ${compo.district}`
+        if (this.props.resultId) {
+          document.getElementById(this.props.resultId).innerHTML = address
+        }
+      }
+
+      /* 逆地理编码 */
+      const regeocoder = () => {
+        const geocoder = new AMap.Geocoder({
+          radius: 1000,
+          extensions: 'all'
+        })
+        geocoder.getAddress(lnglatXY, (status, result) => {
+          console.log(status, result)
+          if (status === 'complete' && result.info === 'OK') {
+            geocoderCallBack(result)
+          }
+        })
+        /* 加点 */
+        const marker = new AMap.Marker({
+          map,
+          position: lnglatXY
+        })
+        // map.setFitView()
+      }
+      regeocoder()
     }
   }
 
   componentWillMount() {
-    const script = document.createElement('script')
-    script.src = 'http://webapi.amap.com/maps?v=1.3&key=db48eaf98740f0ea550863860b3aab81&plugin=AMap.Geocoder'
-    script.async = true
-    script.onload = this.fire
-    document.body.appendChild(script)
+    setTimeout(() => {
+      const script = document.createElement('script')
+      script.src = 'http://webapi.amap.com/maps?v=1.3&key=db48eaf98740f0ea550863860b3aab81&plugin=AMap.Geocoder'
+      script.async = true
+      script.onload = this.fire
+      document.body.appendChild(script)
+    }, 500)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (this.state !== nextState || nextProps.resultId !== this.props.resultId)
+  }
+
+  componentDidUpdate() {
+    if (this.state.success) this.getMap()
   }
 
   render() {
-    const height = this.props.height || 240
-    const width = this.props.height || 240
+    console.log('map render')
+    const height = this.props.height || 360
+    const width = this.props.height || 360
     return (
       <div
         style={{
@@ -69,7 +90,7 @@ class Map extends React.Component {
       >
         {
           this.state.success &&
-            <divi style={{ height, width }} id="mapContainer" />
+            <div style={{ height, width }} id="mapContainer" />
         }
       </div>
     )
